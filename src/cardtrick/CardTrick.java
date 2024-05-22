41876e1809465a3d1afba36d8a0ec03d34b2b62c
ekenenwume <nwume@sheridancollege.ca>
2024-05-22 19:29:46 -0400

    second commit

--- src/cardtrick/CardTrick.java
+++ src/cardtrick/CardTrick.java
@@ -23,31 +23,33 @@ public class CardTrick {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-    Card[] magicHand = new Card[7];
-    Random rand = new Random();
-    
-    for (int i = 0; i < magicHand.length; i++) {
-        Card c = new Card();
-        c.setValue(rand.nextInt(13) + 1); // Card values 1-13
-        c.setSuit(Card.SUITS[rand.nextInt(4)]); // Random suit
-        magicHand[i] = c;
-    }
-    
-    // Hard-coded lucky card
-    Card luckyCard = new Card();
-    luckyCard.setValue(7); // Example value
-    luckyCard.setSuit("Hearts"); // Example suit
-    
-    boolean found = false;
-    for (Card card : magicHand) {
-        if (card.getValue() == luckyCard.getValue() && card.getSuit().equalsIgnoreCase(luckyCard.getSuit())) {
-            found = true;
-            break;
+        Card[] magicHand = new Card[7];
+        Random rand = new Random();
+
+        for (int i = 0; i < magicHand.length; i++) {
+            Card c = new Card();
+            c.setValue(rand.nextInt(13) + 1); // Card values 1-13
+            c.setSuit(Card.SUITS[rand.nextInt(4)]); // Random suit
+            magicHand[i] = c;
+        }
+
+        // Hard-coded lucky card
+        Card luckyCard = new Card();
+        luckyCard.setValue(7); // Example value
+        luckyCard.setSuit("Hearts"); // Example suit
+
+        boolean found = false;
+        for (Card card : magicHand) {
+            if (card.getValue() == luckyCard.getValue() && card.getSuit().equalsIgnoreCase(luckyCard.getSuit())) {
+                found = true;
+                break;
+            }
+        }
+
+        if (found) {
+            System.out.println("Congratulations! The lucky card is in the magic hand.");
+        } else {
+            System.out.println("Sorry, the lucky card is not in the magic hand.");
         }
     }
-    
-    if (found) {
-        System.out.println("Congratulations! The lucky card is in the magic hand.");
-    } else {
-        System.out.println("Sorry, the lucky card is not in the magic hand.");
-    }
+}
